6db5e78045df47af5d45e8a47e0e7facb1016ba9
Alvaro <alvarorfj@gmail.com>
2022-09-21 00:03:41 -0300

    finalizando primeira parte da aplicacao

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Flex, Button, Stack, FormLabel, FormControl} from "@chakra-ui/react";
+import { Input } from "../components/Form/Input";
 
-export default function Home() {
+export default function SignIn() {
   return (
-    <h1>Teste</h1>
-  )
+    <Flex
+      w="100vw"
+      h="100vh"
+      alignItems="center"
+      justifyContent="center">
+        <Flex
+          as="form"
+          width="100%"
+          maxWidth={360}
+          bg="gray.800"
+          p="8"
+          borderRadius={8}
+          flexDirection="column"
+        >
+          <Stack spacing={4}>
+          <Input name="email" type="email" label="E-mail"></Input>
+          <Input name="password" type="password" label="Senha"></Input>
+          </Stack>
+          <Button type="submit" marginTop={6} colorScheme="pink" size="lg">Entrar</Button>
+      </Flex>
+    </Flex>
+  );
 }
